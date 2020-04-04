067727ec744983db3e74bfab50c7aca8dfccc492
dokienko <sncode@yandex.ru>
2020-04-04 18:15:58 +0300

    Intermediate stage of development

--- goto.sh
+++ goto.sh
@@ -5,29 +5,28 @@ source ${HOME}/MyScripts/tmp/vars.env
 get_ssh_user(){
   if [[ $(grep "$1" ${GENERAL_INVENTORY}) == *"Bitrix"* ]]
   then
-    ssh_user=root
+    echo root
   else
-    ssh_user="${SSH_LOGIN}"
+    echo "${SSH_LOGIN}"
   fi
 }
 
 # server_name (csXX-client_id)
 if [[ "$1" == *"cs"* ]]
 then
-  server_ip=$(${GENERAL_INVENTORY} $(echo "$1" | cut -d '-' -f 2) | grep "$1" | cut -d ':' -f 2 | cut -d ' ' -f 2)
+  server_ip=$(cat ${GENERAL_INVENTORY} | grep "$1" | grep ansible_ssh_host | cut -d '=' -f 2 | cut -d ' ' -f 1)
 # ip
 elif [[ $(expr match "$1" "[0-9]*\.[0-9]*") -ne 0 ]]
 then
   server_ip="$1"
 # client_id
 else
-  server_ip=$(cat ${GENERAL_INVENTORY} | grep "$1" | cut -d ':' -f 2 | cut -d ' ' -f 2)
+  server_ip=$(cat ${GENERAL_INVENTORY} | grep "$1" | grep ansible_ssh_host | cut -d '=' -f 2 | cut -d ' ' -f 1)
 fi
 
-if [[ $(echo $server_ip | grep ' ' | wc -l) -eq "0" ]]
+if [[ $(echo "$server_ip" | wc -l) -eq "1" ]]
 then
-  get_ssh_user $server_ip
-  ssh -l $ssh_user $server_ip
+  ssh -l $(get_ssh_user "$server_ip") "$server_ip"
 else
   ~/MyScripts/getcl.sh "$1"
   echo -n 'Enter server name or ip: '
